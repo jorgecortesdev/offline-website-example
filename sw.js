5625b2fb75cf6c93a10c9b262beef64b59755484
Jorge Cortes <jorge.cortes@gmail.com>
2021-03-06 20:38:20 -0700

    first commit

---
+++ sw.js
@@ -0,0 +1,34 @@
+const cacheName = 'my-cache';
+
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/offline.html',
+    '/styles.css',
+    '/bulb.svg'
+];
+
+self.addEventListener('activate', event => self.clients.claim());
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => cache.addAll(filesToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+
+    event.respondWith(
+        fetch(req)
+            .then(response => { return response })
+            .catch(e => {
+                return caches.match(
+                    req.headers.get('Accept').includes('text/html')
+                        ? 'offline.html'
+                        : new Request(req)
+                )
+            })
+    );
+});
